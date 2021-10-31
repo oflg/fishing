9a49496afdd2a53f288668873a188256eed83fe8
Lin Onetwo <linonetwo012@gmail.com>
2021-11-01 01:45:54 +0800

    feat: add fishingTag

--- tiddlers/system/exciseqa.js
+++ tiddlers/system/exciseqa.js
@@ -13,6 +13,7 @@ exports['exciseqa'] = function (event, operation) {
   if (editTiddler && editTiddler.fields['draft.of']) {
     editTiddlerTitle = editTiddler.fields['draft.of'];
   }
+  const fishingTag = event.paramObject.fishingTag || '?';
   // modified here, we use selection as template title, waiting for user to change it to a question
   // and we don't need title param now
   const excisionTitle = this.wiki.generateNewTitle(operation.selection);
@@ -20,12 +21,12 @@ exports['exciseqa'] = function (event, operation) {
     new $tw.Tiddler(this.wiki.getCreationFields(), this.wiki.getModificationFields(), {
       title: excisionTitle,
       text: operation.selection,
-      tags: event.paramObject.tagnew === 'yes' ? [editTiddlerTitle] : [],
+      tags: event.paramObject.tagnew === 'yes' ? [editTiddlerTitle, fishingTag] : [fishingTag],
     })
   );
   // "?" is the default fishing macro
   operation.replacement = `<<${event.paramObject.macro || '?'} """${excisionTitle}""">>`;
-	// delete original texts
+  // delete original texts
   operation.cutStart = operation.selStart;
   operation.cutEnd = operation.selEnd;
   operation.newSelStart = operation.selStart;
