88be8f03f1deb8602f36d3a2cf5fede0b6c45f3c
oflg <oflg@foxmail.com>
2022-03-03 19:48:23 +0800

    add sort random

---
+++ filters/sortrandom.js
@@ -0,0 +1,30 @@
+/*\
+title: $:/plugins/oflg/fishing/filters/sortrandom.js
+type: application/javascript
+module-type: filteroperator
+Filter operator for return a random title in the list.
+\*/
+(function () {
+
+    /*jslint node: true, browser: true */
+    /*global $tw: false */
+    "use strict";
+
+    /*
+    Export our filter function
+    */
+    exports.sortrandom = function (source, operator, options) {
+        var results = [];
+
+        source(function (tiddler, title) {
+            results.push(title);
+        });
+
+        results = results.sort(function () {
+            return Math.random() - 0.5;
+        });
+
+        return results;
+    };
+
+})();
